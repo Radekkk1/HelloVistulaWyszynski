71f3e58b67c9c16e6a74054f6393929c8d017774
radek <radek@example.com>
2022-10-06 18:46:11 +0200

    commit1

---
+++ src/pl/Vistula.java
@@ -0,0 +1,9 @@
+package pl;
+
+public class Vistula {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
